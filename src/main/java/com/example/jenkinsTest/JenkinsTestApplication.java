82800880b1b32370cddb8eecf596747090b03110
lees0980 <leedonggyu@mutecsoft.com>
2023-03-17 14:57:49 +0900

    third commit

--- src/main/java/com/example/jenkinsTest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinsTest/JenkinsTestApplication.java
@@ -8,6 +8,8 @@ public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
+		
+		System.out.println("retest");
 	}
 
 }
