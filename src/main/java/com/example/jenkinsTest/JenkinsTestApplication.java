5673cd73f94c4a637ecf6df03d2e1ae3c4412e70
lees0980 <leedonggyu@mutecsoft.com>
2023-03-17 15:22:49 +0900

    testing..

--- src/main/java/com/example/jenkinsTest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinsTest/JenkinsTestApplication.java
@@ -9,7 +9,7 @@ public class JenkinsTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
 		
-		System.out.println("retest!");
+		System.out.println("retest!!!");
 	}
 
 }
